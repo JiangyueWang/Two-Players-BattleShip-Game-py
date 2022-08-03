a262adc8ad77e0bfa2df51fbd56c429308d7ab4e
Jiang <jiangyue0380@gmail.com>
2022-08-04 09:55:25 +1000

    updated methods in game.py

---
+++ game.py
@@ -0,0 +1,34 @@
+class Game:
+
+    def __init__(self):
+        self.player1 = Player("Player One")
+        self.player2 = Player("Player Two")
+
+        pass
+
+    def display_empty_game_board(self):
+        pass
+
+    def place_ships(self):
+        pass
+
+    def player1_turn(self):
+        pass
+
+    def player2_turn(self):
+        pass
+
+    def check_chosen_location_against_board(self):
+        pass
+
+    def display_updated_game_board(self):
+        pass
+
+    def update_score(self):
+        pass
+
+    def determin_winner(self):
+        pass
+
+    def play_again_check(self):
+        pass
